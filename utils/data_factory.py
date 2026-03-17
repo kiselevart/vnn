4d383628ea9bee3320197418b91c1500f2d0063b
Artem Kiselev <kiselevart1403@gmail.com>
2026-03-17 13:23:21 +0700

    fix: added clamping to flow calc

--- utils/data_factory.py
+++ utils/data_factory.py
@@ -31,7 +31,7 @@ class FlowDatasetWrapper(Dataset):
             flow_video = torch.nan_to_num(flow_video, nan=0.0, posinf=1e3, neginf=-1e3)
 
         # optional safety clamp to prevent huge activations in Volterra terms
-        # flow_video = flow_video.clamp(min=-50.0, max=50.0).float()
+        flow_video = flow_video.clamp(min=-50.0, max=50.0).float()
 
         # Return tuple (rgb, flow), label
         # In the training loop, we check if input is a list/tuple
